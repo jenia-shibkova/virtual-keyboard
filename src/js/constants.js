2e9ccf30173fa3f3d597e1501c33de69b1d2f8cc
Eugenia Shibkova <shibkova91@gmail.com>
2020-03-09 22:16:14 +0300

    fea: add project contants

---
+++ src/js/constants.js
@@ -0,0 +1,12 @@
+const CONSTANTS = {
+  notForPrintButtons = [
+    'CapsLock', 'ShiftLeft', 'ShiftRight', 'AltLeft', 'AltRight', 'MetaLeft', 'ControlLeft', 'ControlRight'
+  ],  
+  notForPressButtons = [
+    'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Pause', 'Insert',
+    'Home', 'PageUp', 'PageDown', 'End', 'NumLock', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'Numpad7',
+    'Numpad8', 'Numpad9', 'NumpadAdd', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad1', 'Numpad2', 'Numpad3', 'Numpad0',
+    'NumpadDecimal', 'NumpadEnter', 'ContextMenu'
+  ]
+};
+
---
+++ src/js/data.js
@@ -0,0 +1,78 @@
+const DATA = [
+  [
+    { name: 'Backquote', value: [['`', '~'], ['ё', 'Ё']] },
+    { name: 'Digit1', value: [['1', '!'], ['1', '!']] },
+    { name: 'Digit2', value: [['2', '!'], ['2', '@']] },
+    { name: 'Digit3', value: [['3', '#'], ['3', '№']] },
+    { name: 'Digit4', value: [['4', '$'], ['4', ';']] },
+    { name: 'Digit5', value: [['5', '%'], ['5', '%']] },
+    { name: 'Digit6', value: [['6', '^'], ['6', ':']] },
+    { name: 'Digit7', value: [['7', '&'], ['7', '?']] },
+    { name: 'Digit8', value: [['8', '*'], ['8', '*']] },
+    { name: 'Digit9', value: [['9', '('], ['9', '(']] },
+    { name: 'Digit0', value: [['0', ')'], ['0', ')']] },
+    { name: 'Minus', value: [['-', '_'], ['-', '_']] },
+    { name: 'Equal', value: [['=', '+'], ['=', '+']] },
+    { name: 'Backspace', value: [['Backspace', 'Backspace'], ['Backspace', 'Backspace']] }
+  ],
+  [
+    { name: 'Tab', value: [['Tab', 'Tab'], ['Tab', 'Tab']] },
+    { name: 'KeyQ', value: [['q', 'Q'], ['й', 'Й']] },
+    { name: 'KeyW', value: [['w', 'W'], ['ц', 'Ц']] },
+    { name: 'KeyE', value: [['e', 'E'], ['у', 'У']] },
+    { name: 'KeyR', value: [['r', 'R'], ['к', 'К']] },
+    { name: 'KeyT', value: [['t', 'T'], ['е', 'Е']] },
+    { name: 'KeyY', value: [['y', 'Y'], ['н', 'Н']] },
+    { name: 'KeyU', value: [['u', 'U'], ['г', 'Г']] },
+    { name: 'KeyI', value: [['i', 'I'], ['ш', 'Ш']] },
+    { name: 'KeyO', value: [['o', 'O'], ['щ', 'Щ']] },
+    { name: 'KeyP', value: [['p', 'P'], ['з', 'З']] },
+    { name: 'BracketLeft', value: [['[', '{ '], ['х', 'Х']] },
+    { name: 'BracketRight', value: [[']', '}'], ['ъ', 'Ъ']] },
+    { name: 'Backslash', value: [['\\', '|'], ['\\', '/']] },
+    { name: 'Delete', value: [['Del', 'Del'], ['Del', 'Del']] }
+  ],
+  [
+    { name: 'CapsLock', value: [['Caps Lock', 'Caps Lock'], ['Caps Lock', 'Caps Lock']] },
+    { name: 'KeyA', value: [['a', 'A'], ['ф', 'Ф']] },
+    { name: 'KeyS', value: [['s', 'S'], ['ы', 'Ы']] },
+    { name: 'KeyD', value: [['d', 'D'], ['в', 'В']] },
+    { name: 'KeyF', value: [['f', 'F'], ['а', 'А']] },
+    { name: 'KeyG', value: [['g', 'G'], ['п', 'П']] },
+    { name: 'KeyH', value: [['h', 'H'], ['р', 'Р']] },
+    { name: 'KeyJ', value: [['j', 'J'], ['о', 'О']] },
+    { name: 'KeyK', value: [['k', 'K'], ['л', 'Л']] },
+    { name: 'KeyL', value: [['l', 'L'], ['д', 'Д']] },
+    { name: 'Semicolon', value: [[';', ':'], ['ж', 'Ж']] },
+    { name: 'Quote', value: [['\'', '"'], ['э', 'Э']] },
+    { name: 'Enter', value: [['Enter', 'Enter'], ['Enter', 'Enter']] }
+  ],
+  [
+    { name: 'ShiftLeft', value: [['Shift', 'Shift'], ['Shift', 'Shift']] },
+    { name: 'KeyZ', value: [['z', 'Z'], ['я', 'Я']] },
+    { name: 'KeyX', value: [['x', 'X'], ['ч', 'Ч']] },
+    { name: 'KeyC', value: [['c', 'C'], ['с', 'С']] },
+    { name: 'KeyV', value: [['v', 'V'], ['м', 'М']] },
+    { name: 'KeyB', value: [['b', 'B'], ['и', 'И']] },
+    { name: 'KeyN', value: [['n', 'N'], ['т', 'Т']] },
+    { name: 'KeyM', value: [['m', 'M'], ['ь', 'Ь']] },
+    { name: 'Comma', value: [[',', '<'], ['б', 'Б']] },
+    { name: 'Period', value: [['.', '>'], ['ю', 'Ю']] },
+    { name: 'Slash', value: [['/', '?'], ['.', ',']] },
+    { name: 'ArrowUp', value: [['▲', '▲'], ['▲', '▲']] },
+    { name: 'ShiftRight', value: [['Shift', 'Shift'], ['Shift', 'Shift']] }
+  ],
+  [
+    { name: 'ControlLeft', value: [['Ctrl', 'Ctrl'], ['Ctrl', 'Ctrl']] },
+    { name: 'MetaLeft', value: [['Win', 'Win'], ['Win', 'Win']] },
+    { name: 'AltLeft', value: [['Alt', 'Alt'], ['Alt', 'Alt']] },
+    { name: 'Space', value: [['', ''], ['', '']] },
+    { name: 'AltRight', value: [['Alt', 'Alt'], ['Alt', 'Alt']] },
+    { name: 'ArrowLeft', value: [['◄', '◄'], ['◄', '◄']] },
+    { name: 'ArrowDown', value: [['▼', '▼'], ['▼', '▼']] },
+    { name: 'ArrowRight', value: [['►', '►'], ['►', '►']] },
+    { name: 'ControlRight', value: [['Ctrl', 'Ctrl'], ['Ctrl', 'Ctrl']] }
+  ]
+];
+
+export default data;

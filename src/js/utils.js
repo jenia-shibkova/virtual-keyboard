a8c68422f1ffa674058f3cdf2bd8f3a71b559cb9
Eugenia Shibkova <shibkova91@gmail.com>
2020-04-07 16:47:14 +0300

    feat: add util function saveState

--- src/js/utils.js
+++ src/js/utils.js
@@ -30,8 +30,17 @@ const switcher = (state, keysA, keysB) => {
   }
 };  
 
+const saveState = (state) => {
+  if (Object.keys(localStorage).includes('lang-en')) {
+    localStorage.clear();
+    localStorage.setItem('lang-en', state);
+  }
+  localStorage.setItem('lang-en', state);
+};
+
 const UTILS = {
   switcher,
+  saveState
 };
 
 export default UTILS;

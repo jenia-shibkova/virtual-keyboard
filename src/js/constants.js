aeee1ce1d0f31881b7dde4d0d5f05312712ef7b8
Eugenia Shibkova <shibkova91@gmail.com>
2020-04-07 15:16:24 +0300

    refactor: refactor constant names

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,12 +1,24 @@
 const CONSTANTS = {
-  notForPrintButtons = [
+  NOT_FOR_PRINT_BUTTONS: [
     'CapsLock', 'ShiftLeft', 'ShiftRight', 'AltLeft', 'AltRight', 'MetaLeft', 'ControlLeft', 'ControlRight'
   ],  
-  notForPressButtons = [
+  NOT_FOR_PRESS_BUTTONS: [
     'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Pause', 'Insert',
     'Home', 'PageUp', 'PageDown', 'End', 'NumLock', 'NumpadDivide', 'NumpadMultiply', 'NumpadSubtract', 'Numpad7',
     'Numpad8', 'Numpad9', 'NumpadAdd', 'Numpad4', 'Numpad5', 'Numpad6', 'Numpad1', 'Numpad2', 'Numpad3', 'Numpad0',
     'NumpadDecimal', 'NumpadEnter', 'ContextMenu'
-  ]
+  ],
+  KEY_CODES: {
+    SHIFT: 16,
+    CAPS_LOCK: 20,
+    TAB: 9,
+    ALT: 18,
+    ARROW_LEFT: 37,
+    ARROW_RIGHT: 39,
+    ARROW_UP: 38,
+    ARROW_DOWN: 40
+  }
 };
+
+export default CONSTANTS;
 
